9773e0647dade8dd2fd8128a6df231e55125331f
Rishi Marodia <98044183+Rishi2602@users.noreply.github.com>
2022-08-18 15:03:47 +0530

    add initial files

---
+++ javascript/main.js
@@ -0,0 +1,95 @@
+
+var submit=document.getElementById("todo-btn")
+var del=document.getElementById("del-btn")
+var todocont=document.getElementById("todo")
+var task=document.getElementById("task")
+var count=0
+
+function Submit()
+{
+if(task.value==""){
+    var m = "It looks like you haven't entered the title";
+    Swal.fire({
+        position:'center',
+        icon: 'error',
+        text:m,
+        title:'Oops!',
+        timer: 3000
+        })
+}
+else{
+count++
+Create(task.value)
+} 
+}
+submit.addEventListener("click",Submit)
+function Create(todo){
+    var taskdiv=document.createElement("div");
+    taskdiv.className=" my-2 py-3 px-2 whole form-group"
+    taskdiv.innerHTML+= count+ ")" + " "+ `<div>
+    <input value="${todo}" id="input${count}" class="input-title">
+    <button class="btn btn-md" id="edit${count}" onclick="update(${count})"><i class="fas fa-edit"></i></button>
+    <button class="btn btn-md" id="save${count}" onclick="save(${count})"><i class="fas fa-save"></i></button>
+    <button class="btn btn-md" id="del${count}" onclick="destroy(${count})"><i class="fas fa-trash"></i></button>
+    </div>`
+    taskdiv.id=count
+   todocont.appendChild(taskdiv)
+   task.value="";
+   var m = "Your Todo has been added.";
+   Swal.fire({
+       position:'center',
+       icon: 'success',
+       text:m,
+       title:'Added!',
+       timer: 3000
+       })
+}
+function destroy(id){
+    document.getElementById(id).remove()
+    var m = "Your Todo has been deleted.";
+    Swal.fire({
+        position:'center',
+        icon: 'success',
+        text:m,
+        title:'Deleted!',
+        timer: 3000
+        })
+}
+del.addEventListener("click",DeleteAll)
+function DeleteAll(){
+    for(var i=1;i<=count;i++)
+    {
+        if(document.getElementById(i))
+        document.getElementById(i).remove()
+        else
+        continue;
+    }
+    count=0;
+    var m = "Your All Todo has been deleted.";
+    Swal.fire({
+        position:'center',
+        icon: 'success',
+        text:m,
+        title:'Deleted!',
+        timer: 3000
+        })
+}
+function update(id){
+    var input=document.getElementById("input"+id)
+    var temp=input.value
+    task.value=temp
+    }    
+
+function save(id){
+    var input=document.getElementById("input"+id)
+    input.value=task.value
+    var m = "Your Todo has been updated successfully.";
+    Swal.fire({
+        position:'center',
+        icon: 'success',
+        text:m,
+        title:'Updated!',
+        timer: 3000
+        })
+        task.value=""
+}
